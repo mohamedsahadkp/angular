014783ba8c078edf21485e3646355d49707116c2
Mohamed Sahad K P <mohamed.sahad@experionglobal.com>
2018-10-04 21:51:33 +0530

    used directive ngFor

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector : 'course',
-  template : '<h2>{{ title + " --- "+ getDescription() }}</h2>'
+  template : `
+      <h2>{{ title }}</h2>
+      <h4>{{ getDescription() }}</h4>
+      <ul>
+        <li *ngFor="let course of courses"> {{ course }} 
+        </li>
+      </ul>
+  `
 })
 
 export class CoursesComponent {
-  title = "My Course"
+  title = "My Course";
+  courses = ["Course1", "Course2", "Course3"];
 
   getDescription = () => {
      return "my course description"

56c48cad5c36c8c21b8ebd92ff2745cc72014224
Mohamed Sahad K P <mohamed.sahad@experionglobal.com>
2018-10-16 22:16:42 +0530

    create a custom pipe

--- example/src/app/star/star.pipe.ts
+++ example/src/app/star/star.pipe.ts
@@ -7,17 +7,26 @@ export class StarPipe implements PipeTransform {
     transform(value: string) {
         if(!value) return null
 
-        let prePosition = [
-            'of', 'the', 'on', 'is', 'was'
-        ];
         let words = value.split(' ');
-        for (let i = 0; i <= words.length ; i++) {
-            if(prePosition.includes(words[i])) {
-                words[i] = words[i].toLowerCase();
+        for (let i = 0; i < words.length ; i++) {
+            let word = words[i];
+            if(this.isPreposition(word)) {
+                words[i] = word.toLowerCase();
             } else {
-                words[i] = words[i].substr(0, 1).toUpperCase(); + words[i].substr(1).toLowerCase();
+                words[i] = this.toTitleCase(word);
             }
         }
         return words.join(' ');
     }
+
+    private toTitleCase(word: string): string {
+        return word.substr(0, 1).toUpperCase() + word.substr(1, word.length).toLowerCase();
+    }
+
+    private isPreposition(word: string): boolean {
+        let prePosition = [
+            'of', 'the', 'on', 'is', 'was'
+        ];
+        return prePosition.includes(word.toLowerCase());
+    }   
 }

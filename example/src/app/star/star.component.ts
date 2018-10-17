8a3ef7bc1f7332e15f8e44e8055e371b9b93dffd
Mohamed Sahad K P <mohamed.sahad@experionglobal.com>
2018-10-17 22:30:46 +0530

    added style in components

--- example/src/app/star/star.component.ts
+++ example/src/app/star/star.component.ts
@@ -1,19 +1,15 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-star',
   templateUrl: './star.component.html',
-  styleUrls: ['./star.component.css']
+  styleUrls: ['./star.component.css'],
 })
-export class StarComponent implements OnInit {
+export class StarComponent {
   @Input('isFavorite') isSeleted: boolean;
   @Output('change') click = new EventEmitter();
   
   constructor() { }
-
-  ngOnInit() {
-    
-  }
 
   onClick() {
     this.isSeleted = !this.isSeleted;
